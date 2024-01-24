79467cc4054b4521fe5ce1ba4bc13a1bcd98e0a2
Steven Chen <steven.chen@soohoobook.com>
2024-01-24 17:15:43 +0800

    chore: update todo item spec

--- src/Domain/Entities/TodoItem.cs
+++ src/Domain/Entities/TodoItem.cs
@@ -2,10 +2,10 @@ namespace Domain.Entities;
 
 public class TodoItem
 {
-    public string Id { get; set; }
+    public int Id { get; set; }
     public string TaskName { get; set; }
     public bool Done  { get; set; }
-    public DateTime CompleteOn  { get; set; }
+    public DateTime? CompleteOn  { get; set; }
     public DateTime CreateOn  { get; set; }
-    public DateTime UpdateOn  { get; set; }
+    public DateTime? UpdateOn  { get; set; }
 }
