1eb9eb34898463144d2a3f4c0b25daa676891469
Steven Chen <steven.chen@soohoobook.com>
2024-01-24 17:15:01 +0800

    chore: interact data with ef core

--- src/Application/UseCase/TodoItem/Commands/CreateTodoItem/CreateTodoItemCommand.cs
+++ src/Application/UseCase/TodoItem/Commands/CreateTodoItem/CreateTodoItemCommand.cs
@@ -1,3 +1,4 @@
+using Application.Common.Interfaces;
 using MediatR;
 
 namespace Application.UseCase.TodoItem.Commands.CreateTodoItem;
@@ -9,8 +10,20 @@ public class CreateTodoItemCommand : IRequest<string>
 
 public class CreateTodoItemCommandHandler : IRequestHandler<CreateTodoItemCommand, string>
 {
+    private readonly ITemplateContext _dataContext;
+
+    public CreateTodoItemCommandHandler(ITemplateContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
     public async Task<string> Handle(CreateTodoItemCommand request, CancellationToken cancellationToken)
     {
+        await _dataContext.TodoItems.AddAsync(new Domain.Entities.TodoItem
+        {
+            TaskName = request.Name,
+            Done = false,
+            CreateOn = DateTime.UtcNow,
+        }, cancellationToken);
         return $"create todo item with name {request.Name}";
     }
 }
--- src/Application/UseCase/TodoItem/Queries/GetTodoItemQuery.cs
+++ src/Application/UseCase/TodoItem/Queries/GetTodoItemQuery.cs
@@ -1,4 +1,6 @@
+using Application.Common.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.UseCase.TodoItem.Queries;
 
@@ -6,10 +8,21 @@ public class GetTodoItemQuery : IRequest<string>
 {
     public int Id { get; set; }
 }
+
 public class GetTodoItemQueryHandler : IRequestHandler<GetTodoItemQuery, string>
 {
+    private readonly ITemplateContext _dataContext;
+
+    public GetTodoItemQueryHandler(ITemplateContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
     public async Task<string> Handle(GetTodoItemQuery request, CancellationToken cancellationToken)
     {
-        return $"get todo item {request.Id}";
+        var todoItem = await _dataContext
+            .TodoItems
+            .FirstAsync(x => x.Id == request.Id, cancellationToken);
+        return $"get todo item with Name:{todoItem.TaskName}";
     }
 }
