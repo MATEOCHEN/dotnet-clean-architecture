7ce3c540e10d80c6491f2f463ef89c954752e5ec
Steven Chen <steven.chen@soohoobook.com>
2024-01-25 14:50:35 +0800

    refactor: move test projects into tests folder

---
+++ tests/Domain.UnitTests/GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using NUnit.Framework;
+global using Domain.Entities;
+global using FluentAssertions;
---
+++ tests/Domain.UnitTests/TodoItemTests.cs
@@ -0,0 +1,26 @@
+namespace Domain.UnitTests;
+
+[TestFixture]
+public class TodoItemTests 
+{
+    [Test]
+    public void todo_item_is_complete()
+    {
+        var todoItem = GivenTodoItem(new TodoItem
+        {
+            Id = 1,
+            TaskName = "Give Steven a massage",
+            Done = true,
+            CompleteOn = new DateTime(2024,1,24),
+            CreateOn = new DateTime(2024,1,20),
+            UpdateOn = new DateTime(2024,1,24)
+        });
+
+        todoItem.IsComplete().Should().BeTrue();
+    }
+
+    private static TodoItem GivenTodoItem(TodoItem item)
+    {
+        return item;
+    }
+}
